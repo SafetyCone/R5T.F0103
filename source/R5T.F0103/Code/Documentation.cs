1b880095877db97dc810e818a18e0ee0daf8eb7a
davidcoats <david@getsafetycone.com>
2023-02-01 09:15:21 -0800

    Initial commit.

---
+++ source/R5T.F0103/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0103
+{
+	/// <summary>
+	/// Timing, profiling, and duration measurement functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0103/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.F0103
+{
+    public static class Instances
+    {
+		
+    }
+}
