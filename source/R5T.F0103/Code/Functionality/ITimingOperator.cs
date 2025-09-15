38a57d6409051030a0366c8fa308c0840c4e74be
davidcoats <david@getsafetycone.com>
2025-09-15 10:45:44 -0700

    Interim commit.

--- source/R5T.F0103/Code/Functionality/ITimingOperator.cs
+++ source/R5T.F0103/Code/Functionality/ITimingOperator.cs
@@ -3,108 +3,20 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 
 using R5T.T0132;
+using R5T.T0143;
 
 
 namespace R5T.F0103
 {
     [FunctionalityMarker]
-    public partial interface ITimingOperator : IFunctionalityMarker
+    public partial interface ITimingOperator : IFunctionalityMarker,
+        F10Y.L0001.L000.ITimingOperator
     {
-        public Stopwatch Get_StartedStopwatch()
-        {
-            Stopwatch stopwatch = new Stopwatch();
+#pragma warning disable IDE1006 // Naming Styles
 
-            stopwatch.Start();
+        [Ignore]
+        F10Y.L0001.L000.ITimingOperator _F10Y_L0001_L000 => F10Y.L0001.L000.TimingOperator.Instance;
 
-            return stopwatch;
-        }
-
-        public TimeSpan Get_Duration(Stopwatch stopwatch)
-        {
-            stopwatch.Stop();
-
-            var duration = stopwatch.Elapsed;
-            return duration;
-        }
-
-        public TimeSpan Measure_Duration_OfAction(Action action)
-        {
-            var stopwatch = this.Get_StartedStopwatch();
-
-            action();
-
-            stopwatch.Stop();
-
-            var duration = stopwatch.Elapsed;
-            return duration;
-        }
-
-        public async Task<TimeSpan> Measure_Duration(Task task)
-        {
-            var stopwatch = this.Get_StartedStopwatch();
-
-            await task;
-
-            stopwatch.Stop();
-
-            var duration = stopwatch.Elapsed;
-            return duration;
-        }
-
-        public async Task<TimeSpan> Measure_Duration(Func<Task> action)
-        {
-            var stopwatch = this.Get_StartedStopwatch();
-
-            await action();
-
-            stopwatch.Stop();
-
-            var duration = stopwatch.Elapsed;
-            return duration;
-        }
-
-        public TimeSpan MeasureDuration(Action action)
-        {
-            var stopwatch = this.Get_StartedStopwatch();
-
-            action();
-
-            stopwatch.Stop();
-
-            var duration = stopwatch.Elapsed;
-            return duration;
-        }
-
-        public TOut MeasureDuration<TOut>(
-            Func<TOut> function,
-            out TimeSpan duration)
-        {
-            var stopwatch = this.Get_StartedStopwatch();
-
-            var output = function();
-
-            stopwatch.Stop();
-
-            duration = stopwatch.Elapsed;
-            
-            return output;
-        }
-
-        public TimeSpan InTimingContext(
-            Action action,
-            Action<TimeSpan> durationConsumer)
-        {
-            var duration = this.InTimingContext(action);
-
-            durationConsumer(duration);
-
-            return duration;
-        }
-
-        public TimeSpan InTimingContext(Action action)
-            => this.MeasureDuration(action);
-
-        public Task<TimeSpan> In_TimingContext(Func<Task> action)
-            => this.Measure_Duration(action);
+#pragma warning restore IDE1006 // Naming Styles
     }
 }
